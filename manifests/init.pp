cb63f782f63f12d6b3bdea380ed0c82b9c8a58c2
Patrick Mans <p.mans@rely.nl>
2016-11-09 16:50:21 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,7 +8,7 @@
 class role_web
 {
   # a role includes one or more profiles and at least a 'base' profile
-  include ::profile_base
+  # include ::profile_base
   # include rspec monitor to make rspec acceptance test available to monitor system
-  include ::profile_apache
+  # include ::profile_apache
 }
